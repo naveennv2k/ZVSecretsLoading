18d39f8c2ec5e19fa80a2ea732a854ba367baa24
naveennv2k <78138836+naveennv2k@users.noreply.github.com>
2024-06-05 16:54:11 +0530

    Update LoadSecrets.js

--- LoadSecrets.js
+++ LoadSecrets.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args=process.argv[2];
 console.log(process.argv);
 // run the `ls` command using exec
-exec(`./zv search -k ${process.argv[2]} --name --tags -V --output json`, (err, output) => {
+exec(`./zv search -k ${process.argv[2]} `, (err, output) => {
   if(err){
      
      console.log(err);
