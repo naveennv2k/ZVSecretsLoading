b96285064547de1c29d2d881f2938881cbd77389
naveennv2k <78138836+naveennv2k@users.noreply.github.com>
2024-06-05 16:55:58 +0530

    Update LoadSecrets.js

--- LoadSecrets.js
+++ LoadSecrets.js
@@ -9,9 +9,33 @@ exec(`./zv search -k ${process.argv[2]} `, (err, output) => {
      
      console.log(err);
   }
-   const secretId=output.secretid;
-   console.log(secretId);
-  
+ const lines = output.split('\n');
+  // console.log(lines);
+    
+    for (let i = 2; i < lines.length; i++) {
+        
+        const columns = lines[i].split('│').map(col => col.trim());
+       // console.log(columns);
+        
+        if (columns.length < 2 || columns[0].startsWith('─')) {
+            continue;
+        }
+      //  console.log(columns[1].substring(7,columns[1].length-10));
+        exec(`./zv get -id ${columns[1].substring(7,columns[1].length-10)} --output json --not-safe`, (err, output) => {
+            
+           // console.log(output);
+            const json=JSON.parse(output);
+           // console.log(output);
+          const secretUsername=json.secret.secretData[0].value;
+          const secretpassword=json.secret.secretData[1].value;
+          console.log(secretUsername);
+          console.log(secretpassword);
+     //     core.exportVariable("testName",secretUsername);
+            // Log the cleaned output
+            // cleanOutput.forEach(line => console.log(line));
+        }
+    );
+    }
 
 
 
