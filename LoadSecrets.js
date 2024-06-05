3eadd2050c900d318f1670832dff93b9628f8704
naveennv2k <78138836+naveennv2k@users.noreply.github.com>
2024-06-05 17:01:26 +0530

    Update LoadSecrets.js

--- LoadSecrets.js
+++ LoadSecrets.js
@@ -21,8 +21,8 @@ exec(`./zv search -k ${process.argv[2]} `, (err, output) => {
         if (columns.length < 2 || columns[0].startsWith('─')) {
             continue;
         }
-      //  console.log(columns[1].substring(7,columns[1].length-10));
-        exec(`./zv get -id ${columns[1].substring(7,columns[1].length-10)} --output json --not-safe`, (err, output) => {
+      console.log(columns[1]);
+        exec(`./zv get -id ${columns[1].substring(0,columns[1].length-10)} --output json --not-safe`, (err, output) => {
             if(err){
               console.log(err);
               return ;
